c193fbfdb9e26883a15c679809f77e724229fd3d
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2024-05-30 20:49:53 +0300

    Fix MSVC u8 literal encoding

--- example/src/example_iterator.hpp
+++ example/src/example_iterator.hpp
@@ -6,15 +6,13 @@
 
 inline void example_iterator()
 {
-	const char s[] = u8"Hello world";
+	const char s[] = u8"Строка в UTF-8";
 
 	auto sbegin = unipp::begin(s);
 	auto send = unipp::end(s);
 	send--; // TODO: set `end` to be null terminator automatically inside
 					// `unipp::end` function.
 
-	// Expectd output:
-	// Length: 30
 	std::cout << "Length: " << std::distance(sbegin, send) << '\n';
 }
 
