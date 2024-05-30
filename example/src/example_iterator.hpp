26ab761cef924af10ec8fe8607017c763f6f1783
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2024-05-30 20:18:56 +0300

    Make example iterator usage with std::distance

--- example/src/example_iterator.hpp
+++ example/src/example_iterator.hpp
@@ -1,23 +1,20 @@
 #pragma once
 
 #include <iostream>
-#include <algorithm>
 
 #include <unipp/iterator.hpp>
 
 inline void example_iterator()
 {
-	const char s[] = u8"Привет, world!";
+	const char s[] = u8"Hello world";
 
-	auto begin = unipp::begin(s);
-	auto end = unipp::end(s);
+	auto sbegin = unipp::begin(s);
+	auto send = unipp::end(s);
+	send--; // TODO: set `end` to be null terminator automatically inside
+					// `unipp::end` function.
 
-	std::cout << "Iterating over a string:\n";
-
-	std::for_each(begin, --end, [](auto view)
-		{
-			std::cout << view.str_view() << ": " << view.decode().symbol << '\n';
-		}
-	);
+	// Expectd output:
+	// Length: 30
+	std::cout << "Length: " << std::distance(sbegin, send) << '\n';
 }
 
