2dc67745aac3fb2782f4c243b19958963d894101
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2025-05-17 13:24:31 +0300

    length takes in a str_view rather than char view

--- testbed/src/main.cpp
+++ testbed/src/main.cpp
@@ -8,9 +8,9 @@ int main()
 	constexpr char16_t u16str[] = u"Привет, мир! This is π";
 	constexpr char32_t u32str[] = U"Привет, мир! This is π";
 
-	constexpr size_t u8len = unipp::length(unipp::char8_view(u8str));
-	constexpr size_t u16len = unipp::length(unipp::char16_view(u16str));
-	constexpr size_t u32len = unipp::length(unipp::char32_view(u32str));
+	constexpr size_t u8len = unipp::length(std::string_view(u8str));
+	constexpr size_t u16len = unipp::length(std::u16string_view(u16str));
+	constexpr size_t u32len = unipp::length(std::u32string_view(u32str));
 
 	std::cout << u8len << '\n'
 		<< u16len << '\n'
--- unipp/include/unipp/length.hpp
+++ unipp/include/unipp/length.hpp
@@ -5,9 +5,10 @@
 namespace unipp
 {
 	template<typename CodeUnitT>
-	constexpr size_t length(char_view<CodeUnitT> view)
+	constexpr size_t length(std::basic_string_view<CodeUnitT> str)
 	{
 		size_t count = 0;
+		char_view<CodeUnitT> view(str);
 		while (view.decode() != nullchar)
 		{
 			if (view.decode() == badchar)
