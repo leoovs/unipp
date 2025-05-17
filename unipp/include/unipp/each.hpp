002ce0686d590b0f10fa2f024db5a8d8e267a2a5
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2025-05-17 14:30:37 +0300

    implement eachchar, fix allocator support

--- unipp/include/unipp/each.hpp
+++ unipp/include/unipp/each.hpp
@@ -30,15 +30,15 @@ namespace unipp
 		typename CodeUnitT,
 		typename VectorAllocT = std::allocator<detail::sv_size_t<CodeUnitT>>
 	>
-	constexpr std::vector<detail::sv_size_t<CodeUnitT>> eachindex(
+	constexpr std::vector<detail::sv_size_t<CodeUnitT>, VectorAllocT> eachindex(
 		std::basic_string_view<CodeUnitT> str,
 		const VectorAllocT& alloc = VectorAllocT())
 	{
-		std::vector<detail::sv_size_t<CodeUnitT>> indices;
-		char_view<CodeUnitT> view(str);
+		using size_type = detail::sv_size_t<CodeUnitT>;
 
-		detail::sv_size_t<CodeUnitT> index = 0;
-		while (view.decode() != nullchar)
+		std::vector<size_type, VectorAllocT> indices(alloc);
+		size_type index = 0;
+		for (char_view<CodeUnitT> view(str); view.decode() != nullchar; view = view.next())
 		{
 			if (view.decode() == badchar)
 			{
@@ -46,11 +46,30 @@ namespace unipp
 			}
 			indices.push_back(index);
 			index += view.get_code_unit_count();
-
-			view = view.next();
 		}
 
 		return indices;
+	}
+
+	template<
+		typename CodeUnitT,
+		typename VectorAllocT = std::allocator<char_view<CodeUnitT>>
+	>
+	constexpr std::vector<char_view<CodeUnitT>, VectorAllocT> eachchar(
+		std::basic_string_view<CodeUnitT> str,
+		const VectorAllocT& alloc = VectorAllocT())
+	{
+		std::vector<char_view<CodeUnitT>, VectorAllocT> chars(alloc);
+		for (char_view<CodeUnitT> view(str); view.decode() != nullchar; view = view.next())
+		{
+			if (view.decode() == badchar)
+			{
+				return chars;
+			}
+			chars.push_back(view);
+		}
+
+		return chars;
 	}
 }
 
